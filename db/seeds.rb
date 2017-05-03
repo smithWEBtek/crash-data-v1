3ca5222c69c9747c03d0852875b5ad1ee892ec58
smithwebtek <brad@smithwebtek.com>
2017-05-03 09:19:39 -0400

    update gemfile; add user resource, migration, controller and views; add root path; add dcms rake task; remove scss files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "ted", email: "ted@abc.com", password: "pointer")
+User.create(username: "red", email: "red@abc.com", password: "pointer")
+User.create(username: "ned", email: "ned@abc.com", password: "pointer")
+User.create(username: "fred", email: "fred@abc.com", password: "pointer")
+User.create(username: "bev", email: "bev@abc.com", password: "pointer")
+User.create(username: "bob", email: "bob@abc.com", password: "pointer")
+User.create(username: "max", email: "max@abc.com", password: "pointer")
